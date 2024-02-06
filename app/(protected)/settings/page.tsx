885962484f18f8657b2a10073b76f8bb57f498f9
Gautam-Hegde <gautamatjob@protonmail.com>
2024-02-06 23:46:19 +0530

    add signout

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,12 +1,23 @@
-import { auth } from "@/auth";
+import { auth ,signOut} from "@/auth";
+import { Button } from "@/components/ui/button";
 
 
 const settingsPage = async () => {
   const session = await auth();
 
-  return (<div>
-   
-    {JSON.stringify(session)}
-  </div>);
+  return (
+    <div>
+      {JSON.stringify(session)}
+
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <Button variant={"destructive"}>Logout</Button>
+      </form>
+    </div>
+  );
 };
 export default settingsPage;
