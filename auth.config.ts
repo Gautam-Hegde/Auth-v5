58ffd22fb21781b570f4091c4e7030769f9d2097
Gautam-Hegde <gautamatjob@protonmail.com>
2024-02-07 11:11:35 +0530

    feat: Oauth

--- auth.config.ts
+++ auth.config.ts
@@ -3,9 +3,19 @@ import bcrypt from "bcryptjs"
 import { LoginSchema } from "./schemas"
 import type { NextAuthConfig } from "next-auth"
 import { getUserByEmail } from "./data/user"
+import github from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET 
+    }),
     Credentials({
       async authorize(credentials) {
         const valid = LoginSchema.safeParse(credentials)
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,17 +1,27 @@
 "use client";
-
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "../ui/button";
-
+import { DEFAULT_REDIRECT } from "@/routes";
 export const Social = () => {
+
+  const onClick = (provider: string) => {
+    signIn(provider, { 
+      callbackUrl: DEFAULT_REDIRECT, 
+    })
+  }
+
+
+
+
   return (
     <div className="flex items-center w-full gap-x-1">
       <Button
         size={"lg"}
         className="w-full"
         variant={"outline"}
-        onClick={() => {}}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="h-5 w-5" />
       </Button>
@@ -19,7 +29,7 @@ export const Social = () => {
         size={"lg"}
         className="w-full"
         variant={"outline"}
-        onClick={() => {}}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="h-5 w-5" />
       </Button>
